f22a0cc505d6813c1d2632ebbcb3d939024c4b61
Giedrekor <43093161+Giedrekor@users.noreply.github.com>
2022-03-07 18:03:43 +0200

    14spr

---
+++ 14Sprintas/7.4Assignment/webpack.config.js
@@ -0,0 +1,46 @@
+// Webpack uses this to work with directories
+const path = require("path");
+
+// This is the main configuration object.
+// Here, you write different options and tell Webpack what to do
+module.exports = {
+  // Path to your entry point. From this file Webpack will begin its work
+  entry: "./src/index.js",
+
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "",
+    filename: "bundle.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ["style-loader", "css-loader"],
+      },
+      // Now we apply rule for images
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            // Using file-loader for these files
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on the final bundle. For now, we don't need production's JavaScript
+  // minifying and other things, so let's set mode to development
+  mode: "development",
+};
---
+++ 14Sprintas/8.5Assignment/dist/bundle.js
@@ -0,0 +1,89 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles.scss */ \"./src/styles.scss\");\n/* harmony import */ var _background_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./background.scss */ \"./src/background.scss\");\n\n\n\nvar calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {\n  if (rate) {\n    monthlyRate = rate / 100 / 12;\n  }\n\n  var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));\n  return monthlyPayment;\n};\n\ndocument.getElementById(\"calcBtn\").addEventListener(\"click\", function () {\n  var principal = document.getElementById(\"principal\").value;\n  var years = document.getElementById(\"years\").value;\n  var rate = document.getElementById(\"rate\").value;\n  var monthlyPayment = calculateMonthlyPayment(principal, years, rate);\n  document.getElementById(\"monthlyPayment\").innerHTML = monthlyPayment.toFixed(2);\n});\n\n//# sourceURL=webpack://assignment-6.5/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/background.scss":
+/*!*****************************!*\
+  !*** ./src/background.scss ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://assignment-6.5/./src/background.scss?");
+
+/***/ }),
+
+/***/ "./src/styles.scss":
+/*!*************************!*\
+  !*** ./src/styles.scss ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://assignment-6.5/./src/styles.scss?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
